0ff389490ba564ad375a0a97426fb2dc8c151ed1
Xiangming Xu <xiangming.xu@emr.ac.uk>
2014-03-13 11:43:14 +0000

    Added some infromation

--- align.sh
+++ align.sh
@@ -2,6 +2,7 @@
 # 1 "<command-line>"
 # 1 "/home/groups/science/seq/commands.txt"
 #!/bin/bash
+#Assemble contigs using velveth and generate summary statistics
 velveth P.cact_assembly 45 -fastq -shortPaired -separate P.cact_411_1M_F_trim.fastq P.cact_411_1M_R_trim.fastq
 velvetg P.cact_assembly -long_mult_cutoff 1 -exp_cov 6 -ins_length 700 -cov_cutoff 2 -min_contig_lgth 750
 process_contigs.pl -i P.cact_assembly/contigs.fa -o process_contigs_P.cact.1
