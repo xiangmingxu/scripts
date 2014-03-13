9660f5776b482ec4d25e1e54fbe3e640c2474143
Xiangming Xu <xiangming.xu@emr.ac.uk>
2014-03-13 15:43:42 +0000

    Final test version

--- align.sh
+++ align.sh
@@ -3,14 +3,23 @@
 # 1 "/home/groups/science/seq/commands.txt"
 #!/bin/bash -x
 #Assemble contigs using velveth and generate summary statistics
-WORK_DIR=P.cact_assembly
-HASH_LENGTH=$1
-FORWARD_FILE=P.cact_411_1M_F_trim.fastq
-REVERSE_FILE=P.cact_411_1M_R_trim.fastq
-ASSEMBLY_NAME=P.cact
+#$ -S /bin/bash
+#$ -cwd
+#$ -pe smp 4
+#$ -l virtual_free=4G
 
-echo "Hash lengh  is  #HASH_LENGTH"
+
+WORK_DIR=$TMPDIR
+HASH_LENGTH=$1
+FORWARD_FILE=$2
+REVERSE_FILE=$3
+ASSEMBLY_NAME=$4
+
+echo "Hash lengh  is  $HASH_LENGTH"
+echo "Forward file is =$FORWARD_FILE"
+echo "Reverse file is =$REVERSE_FILE"
+echo "Assembly name is =$ASSEMBLY_NAME"
+
 velveth $WORK_DIR $HASH_LENGTH -fastq -shortPaired -separate $FORWARD_FILE $REVERSE_FILE
 velvetg $WORK_DIR -long_mult_cutoff 1 -exp_cov 6 -ins_length 700 -cov_cutoff 2 -min_contig_lgth 750
 process_contigs.pl -i $WORK_DIR/contigs.fa -o $ASSEMBLY_NAME.$HASH_LENGTH
-rm - R $WORK_DIR
